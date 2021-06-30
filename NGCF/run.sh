ea8297a1a2d093d781ed8d49fdc7c7cf61cbf085
tony.yoo <l22491360@gmail.com>
2021-06-30 12:19:12 +0900

    add requirements and run.sh

---
+++ NGCF/run.sh
@@ -0,0 +1,3 @@
+#python -m pdb NGCF.py --dataset gowalla --regs [1e-5] --embed_size 64 --layer_size [64,64,64] --lr 0.0001 --save_flag 1 --pretrain 0 --batch_size 1024 --epoch 400 --verbose 1 --node_dropout [0.1] --mess_dropout [0.1,0.1,0.1]
+
+python NGCF.py --dataset amazon-book --regs [1e-5] --embed_size 64 --layer_size [64,64,64] --lr 0.0005 --save_flag 1 --pretrain 0 --batch_size 1024 --epoch 200 --verbose 50 --node_dropout [0.1] --mess_dropout [0.1,0.1,0.1]
